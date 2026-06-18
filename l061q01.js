548d59cb10acff4b53453b1533c78ee52b2c844d
Agatha Nunes da Silva <agathanunes.contato@gmail.com>
2026-06-18 10:15:10 -0300

    exemplos WHILE

---
+++ l061q01.js
@@ -0,0 +1,14 @@
+//Desenvolver um programa que mostre o menu de opções da tabela abaixo e peça o código desejado. Mostre
+//como resposta uma mensagem confirmando a escolha do usuário, ou que o número digitado não existe no
+//menu de opções.
+
+console.log("menu de opção:");
+console.log("código - opçao:");
+console.log("1  -incluir:");
+console.log("2  -alterar:");
+console.log("3  -excluir:");
+console.log("4  -pesquisar:");
+console.log("5  -sair:");
+console.log("informe o código referente á ação desejada:");
+cod = 2;
+console.log(`código inserido: ${cod}`);
